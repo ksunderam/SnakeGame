2e1e3778ce80c17cfe04ea9d8efd2c0af5abcb80
kayansunderam <kayansunderam@gmail.com>
2024-04-18 14:59:31 -0700

    Created Venv, Imported

---
+++ snakegame.py
@@ -0,0 +1,14 @@
+"""
+Snake Game!
+
+@author: Kayan Sunderam
+@version: April 2024
+
+"""
+
+import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import cv2
+import random
+import time
