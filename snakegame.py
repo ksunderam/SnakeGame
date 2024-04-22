27d8ff3e0516f3255db3bfd622cad4da48ee2b2b
kayansunderam <kayansunderam@gmail.com>
2024-04-22 13:43:42 -0700

    Initial Vars, Library Constants

--- snakegame.py
+++ snakegame.py
@@ -12,3 +12,24 @@ from mediapipe.framework.formats import landmark_pb2
 import cv2
 import random
 import time
+
+# Library Constants
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkPoints = mp.solutions.hands.HandLandmark
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+DrawingUtil = mp.solutions.drawing_utils
+
+class Game:
+    """
+    Main game loop. Runs until the 
+    user presses "q".
+    """
+    def __init__(self):
+        # Load game elements
+        self.score
+
+        self.food
+
+        self.snake
