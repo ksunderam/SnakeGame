87a84155e3f27d920da0c4bdee2788e28577a241
kayansunderam <kayansunderam@gmail.com>
2024-04-24 11:54:16 -0700

    Start Landmark Tracking, Run

--- snakegame.py
+++ snakegame.py
@@ -21,6 +21,19 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 DrawingUtil = mp.solutions.drawing_utils
 
+# class Snake:
+#     """
+#     Main character of game
+#     """
+#     def __init__(self):
+
+
+# class Food:
+#     """
+#     Food class to tally score
+#     """
+#     def __init__(self):
+
 class Game:
     """
     Main game loop. Runs until the 
@@ -28,8 +41,103 @@ class Game:
     """
     def __init__(self):
         # Load game elements
-        self.score
+        # self.score
 
-        self.food
+        # self.food
 
-        self.snake
+        # self.snake
+
+        # Create the hand detector
+        base_options = BaseOptions(model_asset_path='data/hand_landmarker.task')
+        options = HandLandmarkerOptions(base_options=base_options,
+                                                num_hands=2)
+        self.detector = HandLandmarker.create_from_options(options)
+
+        # TODO: Load video
+        self.video = cv2.VideoCapture(1)
+
+    def draw_landmarks_on_hand(self, image, detection_result):
+        """
+        Draws all the landmarks on the hand
+        Args:
+            image (Image): Image to draw on
+            detection_result (HandLandmarkerResult): HandLandmarker detection results
+        """
+        # Get a list of the landmarks
+        hand_landmarks_list = detection_result.hand_landmarks
+        
+        # Loop through the detected hands to visualize.
+        for idx in range(len(hand_landmarks_list)):
+            hand_landmarks = hand_landmarks_list[idx]
+
+            # Save the landmarks into a NormalizedLandmarkList
+            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            hand_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+            ])
+
+            # Draw the landmarks on the hand
+            DrawingUtil.draw_landmarks(image,
+                                       hand_landmarks_proto,
+                                       solutions.hands.HAND_CONNECTIONS,
+                                       solutions.drawing_styles.get_default_hand_landmarks_style(),
+                                       solutions.drawing_styles.get_default_hand_connections_style())
+            
+    def run(self):
+        """
+        Main game loop. Runs until the 
+        user presses "q".
+        """    
+        # TODO: Modify loop condition  
+        while self.video.isOpened():
+            # Get the current frame
+            frame = self.video.read()[1]
+
+            # Convert it to an RGB image
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # The image comes in mirrored - flip it
+            image = cv2.flip(image, 1)
+
+
+            # Draw score onto screen
+            cv2.putText(image, str(self.score), (50, 50), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=GREEN, thickness=2)
+
+            # Convert the image to a readable format and find the hands
+            to_detect = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+            results = self.detector.detect(to_detect)
+
+            #Draw the enemy on the image
+            self.green_enemy.draw(image)
+
+            self.red_enemy.draw(image)
+
+            # if time.time() - self.starttime >= 2:
+            #     self.enemies.append(Enemy(GREEN))
+            #     self.enemies.append(Enemy(RED))
+                
+            #     self.starttime = time.time()
+
+            # for enemy in self.enemies:
+            #     enemy.draw(image)
+
+            # Draw the hand landmarks
+            #self.draw_landmarks_on_hand(image, results)
+            self.check_enemy_kill(image, results)
+
+            # Change the color of the frame back
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            cv2.imshow('Hand Tracking', image)
+
+            # Break the loop if the user presses 'q'
+            if cv2.waitKey(50) & 0xFF == ord('q'):
+                print(self.score)
+                break
+
+        self.video.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":        
+    g = Game()
+    g.run()
