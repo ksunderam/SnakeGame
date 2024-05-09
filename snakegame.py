01e6fd81451d3b96fc83d979c44a44398f598b2b
kayansunderam <kayansunderam@gmail.com>
2024-05-09 01:00:31 -0700

    Working Game!

--- snakegame.py
+++ snakegame.py
@@ -1,3 +1,4 @@
+
 """
 Snake Game!
 
@@ -11,13 +12,25 @@ from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import cv2
 import pygame
-import sys
 import random
-import time
+# Set up the screen
+GRID_SIZE = 50
+GRID_WIDTH = 14
+GRID_HEIGHT = 14
+SCREEN_WIDTH = GRID_WIDTH * GRID_SIZE
+SCREEN_HEIGHT = GRID_HEIGHT * GRID_SIZE
+# SCREEN_WIDTH = 700
+# SCREEN_HEIGHT = 700
+# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Snake Game")
+image = pygame.image.load("data/Snake_Game_Grid.png")
+screen.blit(image, (0,0))
 
+# Colors
+WHITE = (255, 255, 255)
+BLUE = (2, 51, 166) #BLUEEEEEE
 RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
 # Initialize Pygame
 pygame.init()
@@ -35,13 +48,79 @@ class Snake:
     Main character of game
     """
     def __init__(self):
+        self.length = 1
+        self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+        self.color = BLUE
 
-        #Goes from 0,0 to 14, 14 maps onto x y when mutiplying with the board (by 50 since image is 700x700), is a 2d array
-        self.location
-        #Just int, determines how far back path goes
-        self.length
-        #1d array
-        self.path
+    def get_head_position(self):
+        return self.positions[0]
+
+    def turn(self, point):
+        if self.length > 1 and (point[0] * -1, point[1] * -1) == self.direction:
+            return
+        else:
+            self.direction = point
+
+    def move(self):
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = (((cur[0] + (x * GRID_SIZE)) % SCREEN_WIDTH), (cur[1] + (y * GRID_SIZE)) % SCREEN_HEIGHT)
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        elif self.get_head_position()[0] < 0 or self.get_head_position()[0] >= SCREEN_WIDTH \
+                or self.get_head_position()[1] < 0 or self.get_head_position()[1] >= SCREEN_HEIGHT:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
+    def reset(self):
+        self.length = 1
+        self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+
+    def draw(self, surface):
+        for p in self.positions:
+            r = pygame.Rect((p[0], p[1]), (GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(surface, self.color, r)
+            pygame.draw.rect(surface, WHITE, r, 1)
+
+    def handle_keys(self, xPos, yPos):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.turn(UP)
+                elif event.key == pygame.K_DOWN:
+                    self.turn(DOWN)
+                elif event.key == pygame.K_LEFT:
+                    self.turn(LEFT)
+                elif event.key == pygame.K_RIGHT:
+                    self.turn(RIGHT)
+
+        if xPos > 500 and xPos < 2000 and yPos < 400:
+            self.turn(UP)
+            print("UP")
+            print(xPos)
+            print(yPos)
+        elif xPos > 500 and xPos < 2000 and yPos > 1200:
+            self.turn(DOWN)
+            print("DOWN")
+            print(xPos)
+            print(yPos)
+        elif yPos > 400 and yPos < 1200 and xPos < 500:
+            self.turn(LEFT)
+            print("LEFT")
+            print(xPos)
+            print(yPos)
+        elif yPos > 400 and yPos < 1200 and xPos > 2000:
+            self.turn(RIGHT)
+            print("RIGHT")
+            print(xPos)
+            print(yPos)
 
 
 
@@ -50,17 +129,23 @@ class Food:
     Food class to tally score
     """
     def __init__(self):
-        #randon location on borad, but not where 
-        self.location
+        self.position = (0, 0)
+        self.color = RED
+        self.randomize_position()
 
-    def respawn():
-        Xlocation = random
-        Ylocation = random
-        location[x][y] = random
+    def randomize_position(self):
+        self.position = (random.randint(0, GRID_WIDTH - 1) * GRID_SIZE, random.randint(0, GRID_HEIGHT - 1) * GRID_SIZE)
 
+    def draw(self, surface):
+        r = pygame.Rect((self.position[0], self.position[1]), (GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(surface, self.color, r)
+        pygame.draw.rect(surface, WHITE, r, 1)
 
-
-
+# Directions
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
 
 
 class Game:
@@ -68,8 +153,6 @@ class Game:
     Main game loop. Runs until the 
     user presses "q".
     """
-
-    BACKGROUND_COLOR = (0, 0, 0)
 
 
     def __init__(self):
@@ -81,17 +164,24 @@ class Game:
         # self.snake
 
 
+        self.clock = pygame.time.Clock()
+        # screen.blit(self.image, (0,0))
+
+
         self.HEIGHT = 700
         self.WIDTH = 700
+
+        self.xPos = 0
+        self.yPos = 0
 
         # Create the game window
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption("Snake Game")
 
-        self.image = pygame.image.load("data/Snake_Game_Grid.png")
-        self.screen.blit(self.image, (0,0))
+        # self.image = pygame.image.load("data/Snake_Game_Grid.png")
+        # self.screen.blit(self.image, (0,0))
 
-        self.board
+        #self.board
 
 
 
@@ -142,92 +232,112 @@ class Game:
                 imageHeight, imageWidth = image.shape[:2]
 
                 pixelCoord = DrawingUtil._normalized_to_pixel_coordinates(finger.x, finger.y, imageWidth, imageHeight)
+                # self.xPos = pixelCoord[0]
+                # self.yPos = pixelCoord[1]
                 pixelCoordThumb = DrawingUtil._normalized_to_pixel_coordinates(thumb.x, thumb.y, imageWidth, imageHeight)
 
-                if pixelCoord:
-                    cv2.circle(image, (pixelCoord[0], pixelCoord[1]), 25, GREEN, 5)
+                if pixelCoord is not None:
+                    cv2.circle(image, (pixelCoord[0], pixelCoord[1]), 25, BLUE, 5)
+                    self.xPos = pixelCoord[0]
+                    self.yPos = pixelCoord[1]
+                    # print(self.xPos)
+                    # print(self.yPos)
                     #self.check_enemy_intercept_hitsametime(pixelCoord[0], pixelCoord[1], self.green_enemy, image, time.time())
                 #     # for enemy in self.enemies:
                 #     #     self.check_enemy_intercept(pixelCoord[0], pixelCoord[1], enemy, image)
                 #     self.check_enemy_intercept(pixelCoord[0], pixelCoord[1], self.green_enemy, image)
             #####DRAW FINGER
+
+
+def main():
+    # Set up the game objects
+    snake = Snake()
+    food = Food()
+    game = Game()
+
+    clock = pygame.time.Clock()
+    game.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Snake Game")
+
+    image = pygame.image.load("data/Snake_Game_Grid.png")
+    game.screen.blit(image, (0,0))
+
+
+    # Main game loop
+    while game.video.isOpened() and True:
+        # screen.fill((0, 0, 0))
+        # screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        # pygame.display.set_caption("Snake Game")
+
+        image = pygame.image.load("data/Snake_Game_Grid.png")
+        game.screen.blit(image, (0,0))
+
+        snake.handle_keys(game.xPos, game.yPos)
+        snake.move()
+
+        if snake.get_head_position() == food.position:
+            snake.length += 1
+            food.randomize_position()
+
+        snake.draw(game.screen)
+        food.draw(game.screen)
+
+        # Check if snake hits the edge of the grid
+        if snake.get_head_position()[0] < 0 or snake.get_head_position()[0] >= SCREEN_WIDTH \
+                or snake.get_head_position()[1] < 0 or snake.get_head_position()[1] >= SCREEN_HEIGHT:
+            snake.reset()
+
+        pygame.display.update()
+        clock.tick(10)
+
+        # Get the current frame
+        frame = game.video.read()[1]
+
+        # Convert it to an RGB image
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # The image comes in mirrored - flip it
+        image = cv2.flip(image, 1)
+
+
+
+        # Draw score onto screen
+        # cv2.putText(image, str(self.score), (50, 50), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=GREEN, thickness=2)
+
+        # Convert the image to a readable format and find the hands
+        to_detect = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+        results = game.detector.detect(to_detect)
+
+        #Draw the enemy on the image
+        #self.green_enemy.draw(image)
+
+        #self.red_enemy.draw(image)
+
+        # if time.time() - self.starttime >= 2:
+        #     self.enemies.append(Enemy(GREEN))
+        #     self.enemies.append(Enemy(RED))
             
-    def run(self):
-        """
-        Main game loop. Runs until the 
-        user presses "q".
-        """    
+        #     self.starttime = time.time()
 
-        # Draw the initial screen
-        #self.screen.fill(self.BACKGROUND_COLOR)
-        #self.screen.draw(self.screen)
-        #self.mango.draw(self.screen)
-        self.screen.blit(self.image, (0,0))
-        pygame.display.flip()
+        # for enemy in self.enemies:
+        #     enemy.draw(image)
 
-        # TODO: Modify loop condition  
-        running  = True
-        while self.video.isOpened() and running:
+        # Draw the hand landmarks
+        game.draw_landmarks_on_hand(image, results)
+        #self.check_enemy_kill(image, results)
 
-            self.screen.blit(self.image, (0,0))
-            pygame.display.flip()
+        # Change the color of the frame back
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-            # Get the current frame
-            frame = self.video.read()[1]
+        # cv2.namedWindow('Hand Tracking', cv2.WINDOW_NORMAL)
+        # cv2.resizeWindow('Hand Tracking', 2500, 1600)
 
-            # Convert it to an RGB image
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        #FOR VIDEO TO SHOW - AAAAAAAA
+        #cv2.imshow('Hand Tracking', image)
+        cv2.destroyAllWindows
+        # print(cv2.getWindowImageRect('Hand Tracking'))
 
-            # The image comes in mirrored - flip it
-            image = cv2.flip(image, 1)
+if __name__ == "__main__":
+    main()
 
 
-
-            # Draw score onto screen
-            # cv2.putText(image, str(self.score), (50, 50), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=GREEN, thickness=2)
-
-            # Convert the image to a readable format and find the hands
-            to_detect = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-            results = self.detector.detect(to_detect)
-
-            #Draw the enemy on the image
-            #self.green_enemy.draw(image)
-
-            #self.red_enemy.draw(image)
-
-            # if time.time() - self.starttime >= 2:
-            #     self.enemies.append(Enemy(GREEN))
-            #     self.enemies.append(Enemy(RED))
-                
-            #     self.starttime = time.time()
-
-            # for enemy in self.enemies:
-            #     enemy.draw(image)
-
-            # Draw the hand landmarks
-            self.draw_landmarks_on_hand(image, results)
-            #self.check_enemy_kill(image, results)
-
-            # Change the color of the frame back
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-            #FOR VIDEO TO SHOW - AAAAAAAA
-            cv2.imshow('Hand Tracking', image)
-
-
-            # Break the loop if the user presses 'q'
-            if cv2.waitKey(50) & 0xFF == ord('q'):
-                #print(self.score)
-                break
-
-        self.video.release()
-        cv2.destroyAllWindows()
-        pygame.quit()
-        sys.exit()
-
-
-
-
-if __name__ == "__main__":        
-    g = Game()
-    g.run()
